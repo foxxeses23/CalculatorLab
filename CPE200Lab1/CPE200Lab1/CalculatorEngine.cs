5f5fc30ee833be5238b196836c007a6ab4a5d72e
thanwun <arnuang@gmail.com>
2018-08-24 17:09:54 +0700

    gg level2

---
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    class CalculatorEngine
+
+    {
+        private bool hasDot;
+        private bool isAllowBack;
+        private bool isAfterOperater;
+        private bool isAfterEqual;
+        private string firstOperand;
+        private string operate;
+        public string display = "0";
+
+
+
+        public string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
+        {
+            switch (operate)
+            {
+                case "+":
+                    return (Convert.ToDouble(firstOperand) + Convert.ToDouble(secondOperand)).ToString();
+                case "-":
+                    return (Convert.ToDouble(firstOperand) - Convert.ToDouble(secondOperand)).ToString();
+                case "X":
+                    return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
+                case "÷":
+                    // Not allow devide be zero
+                    if (secondOperand != "0")
+                    {
+                        double result;
+                        string[] parts;
+                        int remainLength;
+
+                        result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
+                        // split between integer part and fractional part
+                        parts = result.ToString().Split('.');
+                        // if integer part length is already break max output, return error
+                        if (parts[0].Length > maxOutputSize)
+                        {
+                            return "E";
+                        }
+                        // calculate remaining space for fractional part.
+                        remainLength = maxOutputSize - parts[0].Length - 1;
+                        // trim the fractional part gracefully. =
+                        return result.ToString("N" + remainLength);
+                    }
+                    break;
+                case "%":
+                    //your code here
+                    break;
+            }
+            return "E";
+        }
+
+        public void handleOperetor(string operate)
+        {
+            if (display is "Error")
+            {
+                return;
+            }
+            if (isAfterOperater)
+            {
+                return;
+            }
+           
+            switch (operate)
+            {
+                case "+":
+                case "-":
+                case "X":
+                case "÷":
+                    firstOperand = display;
+                    isAfterOperater = true;
+                    break;
+                case "%":
+                    // your code here
+                    break;
+            }
+            isAllowBack = false;
+        }
+
+        public void handleDigit(string digit)
+        {
+            if (display is "Error")
+            {
+                return;
+            }
+            if (isAfterEqual)
+            {
+                reset_all();
+            }
+            if (isAfterOperater)
+            {
+                display = "0";
+            }
+            if (display.Length is 8)
+            {
+                return;
+            }
+            isAllowBack = true;
+            if (display is "0")
+            {
+                display = "";
+            }
+            display += digit;
+            isAfterOperater = false;
+        }
+
+        public void reset_all()
+        {
+            display = "0";
+            isAllowBack = true;
+            hasDot = false;
+            isAfterOperater = false;
+            isAfterEqual = false;
+
+        }
+
+        public void dot()
+        {
+            if (!hasDot)
+            {
+                display += ".";
+                hasDot = true;
+            }
+        }
+
+        public void backSpace()
+        {
+            if (display != "0")
+            {
+                string current = display;
+                char rightMost = current[current.Length - 1];
+                if (rightMost is '.')
+                {
+                    hasDot = false;
+                }
+                display = current.Substring(0, current.Length - 1);
+                if (display is "" || display is "-")
+                {
+                    display = "0";
+                }
+            }
+
+        }
+
+        public void sign()
+        {
+            if (isAfterEqual)
+            {
+                reset_all();
+            }
+            // already contain negative sign
+            if (display.Length is 8)
+            {
+                return;
+            }
+            if (display[0] is '-')
+            {
+                display = display.Substring(1, display.Length - 1);
+            }
+            else
+            {
+                display = "-" + display;
+            }
+        }
+
+        public void equal()
+        {
+            string secondOperand = display;
+
+            string result = calculate(operate, firstOperand, secondOperand);
+            if (result is "E" || result.Length > 8)
+            {
+                display = "Error";
+            }
+            else
+            {
+                display = result;
+            }
+            isAfterEqual = true;
+        }
+    }
+}
